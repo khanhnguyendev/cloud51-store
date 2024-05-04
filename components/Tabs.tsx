0e6cff8ea5a9fbbac7bf1fe075a5a9e942d2cad8
nguyen.nk <khanhnguyen.vlu@gmail.com>
2024-05-05 00:43:43 +0700

    #UI update icon

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 import { Image } from "@nextui-org/image";
 import IphonePage from "./IphonePage";
 import { Product } from "@/utils/interface";
+import { IphoneIcon } from "./icons";
 
 type Props = {
   products: Product[];
@@ -23,62 +24,46 @@ const Tabs = ({ products }: Props) => {
       <div className="flex justify-center gap-5">
         <Button
           onClick={() => handleTabClick("iphone")}
-          className={`w-1/3 h-[60px] py-2 px-4 ${activeTab === "iphone" ? "bg-blue-500" : "bg-blue-200"} text-white dark:bg-blue-600 dark:text-white dark:hover:bg-opacity-40 dark:bg-opacity-50 focus:outline-none focus:bg-blue-600 dark:focus:bg-blue-400`}
-          startContent={
-            <Image
-              src="/assets/images/iphone.svg"
-              height={50}
-              width={50}
-              alt="iphone"
-            />
-          }
-          style={{ maxWidth: "150px" }}
+          className={`w-[150px] h-[60px] py-2 px-4 ${activeTab === "iphone" ? "bg-blue-500" : "bg-blue-200"} text-white dark:bg-blue-600 dark:text-white dark:hover:bg-opacity-40 dark:bg-opacity-50 focus:outline-none focus:bg-blue-600 dark:focus:bg-blue-400`}
+          // startContent={<IphoneIcon />}
         >
-          <span className="text-base">
-            {activeTab === "iphone" ? "iPhone" : "iPhone"}
-          </span>
+          <span className="text-base">iPhone</span>
         </Button>
         <Button
           onClick={() => handleTabClick("macbook")}
-          className={`w-1/3 h-[60px] py-2 px-4 ${activeTab === "macbook" ? "bg-blue-500" : "bg-blue-200"} text-white dark:bg-blue-600 dark:text-white dark:hover:bg-opacity-40 dark:bg-opacity-50 focus:outline-none focus:bg-blue-600 dark:focus:bg-blue-400`}
-          startContent={
-            <Image
-              src="/assets/images/macbook.svg"
-              height={50}
-              width={50}
-              alt="macbook"
-            />
-          }
-          style={{ maxWidth: "150px" }}
+          className={`w-[150px] h-[60px] py-2 px-4 ${activeTab === "macbook" ? "bg-blue-500" : "bg-blue-200"} text-white dark:bg-blue-600 dark:text-white dark:hover:bg-opacity-40 dark:bg-opacity-50 focus:outline-none focus:bg-blue-600 dark:focus:bg-blue-400`}
+          // startContent={
+          //   <Image
+          //     src="/assets/images/macbook.svg"
+          //     height={50}
+          //     width={50}
+          //     alt="macbook"
+          //   />
+          // }
         >
-          <span className="text-base">
-            {activeTab === "macbook" ? "Macbook" : "Macbook"}
-          </span>
+          <span className="text-base">Macbook</span>
         </Button>
         <Button
           onClick={() => handleTabClick("ipad")}
-          className={`w-1/3 h-[60px] py-2 px-4 ${activeTab === "ipad" ? "bg-blue-500" : "bg-blue-200"} text-white dark:bg-blue-600 dark:text-white dark:hover:bg-opacity-40 dark:bg-opacity-50 focus:outline-none focus:bg-blue-600 dark:focus:bg-blue-400`}
-          startContent={
-            <Image
-              src="/assets/images/ipad.svg"
-              height={50}
-              width={50}
-              alt="ipad"
-            />
-          }
-          style={{ maxWidth: "150px" }}
+          className={`w-[150px] h-[60px] py-2 px-4 ${activeTab === "ipad" ? "bg-blue-500" : "bg-blue-200"} text-white dark:bg-blue-600 dark:text-white dark:hover:bg-opacity-40 dark:bg-opacity-50 focus:outline-none focus:bg-blue-600 dark:focus:bg-blue-400`}
+          // startContent={
+          //   <Image
+          //     src="/assets/images/ipad.svg"
+          //     height={50}
+          //     width={50}
+          //     alt="ipad"
+          //   />
+          // }
         >
-          <span className="text-base">
-            {activeTab === "ipad" ? "iPad" : "iPad"}
-          </span>
+          <span className="text-base">iPad</span>
         </Button>
       </div>
 
       {/* Tab Content */}
       <div className="p-4 mt-10">
         {activeTab === "iphone" && <IphonePage products={products} />}
-        {activeTab === "macbook" && <p>Chưa có sản phẩm...</p>}
-        {activeTab === "ipad" && <p>Chưa có sản phẩm...</p>}
+        {activeTab === "macbook" && <span>Chưa có sản phẩm...</span>}
+        {activeTab === "ipad" && <span>Chưa có sản phẩm...</span>}
       </div>
     </div>
   );
