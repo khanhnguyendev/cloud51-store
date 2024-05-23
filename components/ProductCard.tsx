e7171df06570e41776964254826fd846651439c8
nguyen.nk <khanhnguyen.vlu@gmail.com>
2024-05-23 15:15:41 +0700

    #UI fix modal

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@nextui-org/button";
 import { Image } from "@nextui-org/image";
 import {
@@ -26,7 +26,13 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ products }: ProductCardProps) => {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  const handleProductClick = (product: Product) => {
+    setSelectedProduct(product);
+    onOpen();
+  };
 
   const handleButtonClick = () => {
     window.open("https://zalo.me/0888393339", "_blank");
@@ -36,7 +42,10 @@ const ProductCard = ({ products }: ProductCardProps) => {
     <>
       {products.map((product) => (
         <div key={product._id}>
-          <Card className="max-w-sm mx-auto shadow-lg rounded-lg overflow-hidden transition-transform transform-gpu duration-300 hover:scale-105 hover:shadow-2xl bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700">
+          <Card
+            className="max-w-sm mx-auto shadow-lg rounded-lg overflow-hidden transition-transform transform-gpu duration-300 hover:scale-105 hover:shadow-2xl bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700"
+            onClick={() => handleProductClick(product)}
+          >
             <CardHeader className="p-3 flex justify-center items-center">
               <CardTitle className="h-[50px] lg:h-[75px] overflow-hidden text-sm lg:text-base font-semibold text-center text-gray-900 dark:text-gray-100">
                 {product.title}
@@ -53,46 +62,46 @@ const ProductCard = ({ products }: ProductCardProps) => {
             </CardContent>
             <CardFooter className="flex flex-col items-center p-4">
               <div className="flex items-center text-xs 2xl:text-base font-bold text-red-500 dark:text-red-400 mb-2">
-                {/* <TagPrice /> */}
                 <span className="ml-1">{product.price.toLocaleString()}</span>
                 <span className="ml-1">VND</span>
               </div>
               <Button
                 color="danger"
-                onPress={onOpen}
+                onPress={() => handleProductClick(product)}
                 className="text-white text-xs bg-red-500 dark:bg-red-600 dark:hover:bg-red-500"
               >
                 Chi Tiết
               </Button>
             </CardFooter>
           </Card>
-
-          <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-            <ModalContent>
-              {(onClose) => (
-                <>
-                  <ModalHeader className="flex flex-col gap-1">
-                    {product.title}
-                  </ModalHeader>
-                  <ModalBody>
-                    <p>
-                      {product.detail || "Vui lòng liên hệ để nhận ưu đãi!!"}
-                    </p>
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button color="danger" variant="light" onPress={onClose}>
-                      Close
-                    </Button>
-                    <Button color="danger" onPress={handleButtonClick}>
-                      Liên hệ
-                    </Button>
-                  </ModalFooter>
-                </>
-              )}
-            </ModalContent>
-          </Modal>
         </div>
       ))}
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          {selectedProduct && (
+            <>
+              <ModalHeader>
+                <h4>{selectedProduct.title}</h4>
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  {selectedProduct.detail ||
+                    "Vui lòng liên hệ để nhận ưu đãi!!"}
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onClick={onClose}>
+                  Close
+                </Button>
+                <Button color="danger" onClick={handleButtonClick}>
+                  Liên hệ
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 };
