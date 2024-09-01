953d447d7c38e70dec61832b66a977824f6dad72
nguyen.nk <44081478+khanhnguyendev@users.noreply.github.com>
2024-09-01 21:39:40 +0700

    Add: vercel analytics

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "Cloud51 CMS",
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
